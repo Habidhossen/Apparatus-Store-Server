951e098661a33cb52a5abe5dbeccffbc29cf094e
Habidhossen <habidhossen2@gmail.com>
2022-05-23 19:53:43 +0600

    get one product api added

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    // GET One Product by ID
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
