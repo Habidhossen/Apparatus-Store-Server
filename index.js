3e85a290710f13d88404a6f2c7b2650652ee2957
Habidhossen <habidhossen2@gmail.com>
2022-05-27 15:00:31 +0600

    delete product api added

--- index.js
+++ index.js
@@ -54,6 +54,14 @@ async function run() {
       res.send(result);
     });
 
+    // DELETE (Product)
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // POST (Review)
     app.post("/review", async (req, res) => {
       const data = req.body;
