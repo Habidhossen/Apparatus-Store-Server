1d1eae11cb24abc53cb97fcd00f0ea6a25fc163c
Habidhossen <habidhossen2@gmail.com>
2022-05-28 16:48:37 +0600

    code updated

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const ObjectId = require("mongodb").ObjectId;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // middleware
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: true,
-//     optionsSuccessStatus: 200,
-//     credentials: true,
-//   })
-// );
+// app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // mongoDB drive code
